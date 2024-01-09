a9ba5ea8a091f86ea3140d2beed036d99de4bae8
Muiz-Eddy <109058675+Muiz-Eddy@users.noreply.github.com>
2024-01-09 21:29:07 +0800

    Modify: Implemented selection sort algorithm

--- algo-typescript/src/sort.ts
+++ algo-typescript/src/sort.ts
@@ -1,1 +1,22 @@
+function findSmallest(arr: number[]): number {
+  let smallest = arr[0]
+  let smallestIndex = 0
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      smallest = arr[i]
+      smallestIndex = i
+    }
+  }
+  return smallestIndex
+}
 
+export function selectionSort(arr: number[]): number[] {
+  const sortedArr: number[] = []
+  const length = arr.length
+  for (let i = 0; i < length; i++) {
+    const index = findSmallest(arr)
+    sortedArr.push(arr[index])
+    arr.splice(index, 1)
+  }
+  return sortedArr
+}
