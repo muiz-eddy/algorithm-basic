dc2c1a9daf73725fba011bcdf5089c3ab46eb8d8
muiz-eddy <am.eddy25@gmail.com>
2024-01-10 03:34:08 +0800

    Added: Skeleton for recursion

---
+++ algo-typescript/src/recursion.ts
@@ -0,0 +1,7 @@
+// Recursion Exercise from https://tildesites.bowdoin.edu/~ltoma/teaching/cs107/spring05/recursion.html
+//
+
+export function sum(n: number): number {
+  // base case
+  // recursive case
+}
