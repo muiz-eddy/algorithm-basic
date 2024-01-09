ae5256f8e7f580e0298dd147addfb6babf9b1e5b
Muiz-Eddy <109058675+Muiz-Eddy@users.noreply.github.com>
2024-01-10 03:08:38 +0800

    Fix: Test suites for sort

--- algo-typescript/tests/sort.test.ts
+++ algo-typescript/tests/sort.test.ts
@@ -1,9 +1,9 @@
-import { selectionSort } from '../src/sort.ts'
+import { selectionSort } from '../src/sort'
 
 const arr: number[] = [2, 1, 4, 6, 5]
 
 describe('sort selections', () => {
   it('should sort the array', () => {
-    expect((arr)).toEqual([1, 2, 4, 5, 6])
+    expect(selectionSort(arr)).toEqual([1, 2, 4, 5, 6])
   })
 })
