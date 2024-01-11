6f48352765926bea4e0b5c8a6aa8394b4c44a251
muiz-eddy <am.eddy25@gmail.com>
2024-01-11 16:02:48 +0800

    Added: Test case for findsum and findmin)

--- algo-typescript/tests/recursion.test.ts
+++ algo-typescript/tests/recursion.test.ts
@@ -1,7 +1,27 @@
-import { sum } from '../src/recursion'
+import { sum, findmin, findsum } from '../src/recursion'
 
 describe('sum function using recursion', () => {
   it('should sum all of the integer from 1 - n', () => {
     expect(sum(10)).toEqual(55)
+  })
+})
+
+const arr: number[] = [4, 8, 3, 4, 5, 6, 2]
+
+describe('find minimum function using recursion', () => {
+  it('should find minimum element in an array', () => {
+    expect(findmin(arr, arr.length)).toEqual(2)
+  })
+})
+
+describe('find minimum function using recursion', () => {
+  it('should return the first element if array size = 1', () => {
+    expect(findmin([2], 1)).toEqual(2)
+  })
+})
+
+describe('find sum function of an array using recursion', () => {
+  it('should return the sum of the elements in an array', () => {
+    expect(findsum(arr, arr.length)).toEqual(32)
   })
 })
